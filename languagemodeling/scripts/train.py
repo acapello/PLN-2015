eefa1708124fcc7431bc71b546af8d759953fd3d
Agustin Capello <capelloagustin@gmail.com>
2015-09-10 18:59:38 -0300

    * Exercise 1

--- languagemodeling/scripts/train.py
+++ languagemodeling/scripts/train.py
@@ -1,31 +1,52 @@
 """Train an n-gram model.
 
 Usage:
-  train.py -n <n> -o <file>
+  train.py -n <n> [-m <model>] -o <file>
   train.py -h | --help
 
 Options:
   -n <n>        Order of the model.
+  -m <model>    Model to use [default: ngram]:
+                  ngram: Unsmoothed n-grams.
+                  addone: N-grams with add-one smoothing.
   -o <file>     Output model file.
   -h --help     Show this screen.
 """
-from docopt import docopt
+import sys
+sys.path.append("../../")
+
 import pickle
-
-from nltk.corpus import gutenberg
-
-from languagemodeling.ngram import NGram
+from docopt import docopt
+from nltk.corpus import PlaintextCorpusReader
+# from nltk.corpus import gutenberg
+from languagemodeling.ngram import NGram, AddOneNGram
 
 
 if __name__ == '__main__':
+
     opts = docopt(__doc__)
 
     # load the data
-    sents = gutenberg.sents('austen-emma.txt')
+    # sents = gutenberg.sents('austen-emma.txt')
+    corpus = PlaintextCorpusReader('../corpora/', 'training_corpus.txt')
+    sents = corpus.sents()
+
+    # order of the model
+    n = int(opts['-n'])
+    # model type
+    m = str(opts['-m'])
 
     # train the model
-    n = int(opts['-n'])
-    model = NGram(n, sents)
+    if m == "ngram":
+        print "NGram Model selected"
+        model = NGram(n, sents)
+    elif m == "addone":
+        print "AddOne Model selected"
+        model = AddOneNGram(n, sents)
+    else:
+        print "Bad Model Type"
+        print help()
+        exit()
 
     # save it
     filename = opts['-o']
