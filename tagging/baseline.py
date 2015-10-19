2e91d35a80d6e772e6af7a20a3b41348e0e0341b
Agustin Capello <capelloagustin@gmail.com>
2015-10-18 21:33:00 -0300

    Exercise 2 - Baseline Tagger

--- tagging/baseline.py
+++ tagging/baseline.py
@@ -1,3 +1,4 @@
+from collections import defaultdict, Counter
 
 
 class BaselineTagger:
@@ -6,25 +7,54 @@ class BaselineTagger:
         """
         tagged_sents -- training sentences, each one being a list of pairs.
         """
-        pass
+        self.voc = set()
+        self.set_of_tags = set()
+        self.tag_freq = Counter()
+        self.most_freq_tag_by_word = defaultdict(str)
+        tagsby_word = defaultdict(lambda: Counter())
 
-    def tag(self, sent):
-        """Tag a sentence.
+        for sent in tagged_sents:
+            for word, tag in sent:
+                self.voc.add(word)
+                self.set_of_tags.add(tag)
+                self.tag_freq[tag] += 1
+                tagsby_word[word][tag] += 1
 
-        sent -- the sentence.
+        for w, tag_dict in tagsby_word.items():
+            self.most_freq_tag_by_word[w] = tag_dict.most_common(1)[0][0]
+
+        self.most_freq_tag = self.tag_freq.most_common(1)[0][0]
+
+    def tagset(self):
+        """Returns the set of tags.
         """
-        return [self.tag_word(w) for w in sent]
-
-    def tag_word(self, w):
-        """Tag a word.
-
-        w -- the word.
-        """
-        return 'nc'
+        return self.set_of_tags
 
     def unknown(self, w):
         """Check if a word is unknown for the model.
 
         w -- the word.
         """
-        return True
+        return w not in self.voc
+
+    def tag_word(self, w):
+        """Tag a word.
+
+        w -- the word.
+        """
+        tag = None
+
+        if self.unknown(w):
+            tag = self.most_freq_tag
+        else:
+            tag = self.most_freq_tag_by_word[w]
+
+        return tag
+
+    def tag(self, sent):
+        """Tag a sentence.
+
+        sent -- the sentence.
+        """
+        tagging = [self.tag_word(w) for w in sent]
+        return tagging
