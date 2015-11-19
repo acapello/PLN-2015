8a6dbe82699388e4715c9c4285c9f4a9bb221877
Agustin Capello <capelloagustin@gmail.com>
2015-11-19 05:38:23 -0300

    bugfix

--- tagging/scripts/eval_all_models.sh
+++ tagging/scripts/eval_all_models.sh
@@ -10,37 +10,37 @@ echo -n > $M/eval_output.txt
 echo "Redirecting output to $M/eval_output.txt"
 echo "Working..."
 
-echo "Training base..."
+echo "Eval base..."
 { time -p python $SCRIPT_DIR/eval.py -i $M/base >> $M/eval_output.txt ;} 2>> $M/eval_output.txt
-echo "Training mlhmm n=1..."
+echo "Eval mlhmm n=1..."
 { time -p python $SCRIPT_DIR/eval.py -i $M/mlhmm-1 >> $M/eval_output.txt ;} 2>> $M/eval_output.txt
-echo "Training mlhmm n=2..."
+echo "Eval mlhmm n=2..."
 { time -p python $SCRIPT_DIR/eval.py -i $M/mlhmm-2 >> $M/eval_output.txt ;} 2>> $M/eval_output.txt
-echo "Training mlhmm n=3..."
+echo "Eval mlhmm n=3..."
 { time -p python $SCRIPT_DIR/eval.py -i $M/mlhmm-3 >> $M/eval_output.txt ;} 2>> $M/eval_output.txt
-echo "Training mlhmm n=4..."
+echo "Eval mlhmm n=4..."
 { time -p python $SCRIPT_DIR/eval.py -i $M/mlhmm-4 >> $M/eval_output.txt ;} 2>> $M/eval_output.txt
-echo "Training memm logreg n=1..."
+echo "Eval memm logreg n=1..."
 { time -p python $SCRIPT_DIR/eval.py -i $M/memm-logreg-1 >> $M/eval_output.txt ;} 2>> $M/eval_output.txt
-echo "Training memm logreg n=2..."
+echo "Eval memm logreg n=2..."
 { time -p python $SCRIPT_DIR/eval.py -i $M/memm-logreg-2 >> $M/eval_output.txt ;} 2>> $M/eval_output.txt
-echo "Training memm logreg n=3..."
+echo "Eval memm logreg n=3..."
 { time -p python $SCRIPT_DIR/eval.py -i $M/memm-logreg-3 >> $M/eval_output.txt ;} 2>> $M/eval_output.txt
-echo "Training memm logreg n=4..."
+echo "Eval memm logreg n=4..."
 { time -p python $SCRIPT_DIR/eval.py -i $M/memm-logreg-4 >> $M/eval_output.txt ;} 2>> $M/eval_output.txt
-echo "Training memm nb n=1..."
+echo "Eval memm nb n=1..."
 { time -p python $SCRIPT_DIR/eval.py -i $M/memm-nb-1 >> $M/eval_output.txt ;} 2>> $M/eval_output.txt
-echo "Training memm nb n=2..."
+echo "Eval memm nb n=2..."
 { time -p python $SCRIPT_DIR/eval.py -i $M/memm-nb-2 >> $M/eval_output.txt ;} 2>> $M/eval_output.txt
-echo "Training memm nb n=3..."
+echo "Eval memm nb n=3..."
 { time -p python $SCRIPT_DIR/eval.py -i $M/memm-nb-3 >> $M/eval_output.txt ;} 2>> $M/eval_output.txt
-echo "Training memm nb n=4..."
+echo "Eval memm nb n=4..."
 { time -p python $SCRIPT_DIR/eval.py -i $M/memm-nb-4 >> $M/eval_output.txt ;} 2>> $M/eval_output.txt
-echo "Training memm svc n=1..."
+echo "Eval memm svc n=1..."
 { time -p python $SCRIPT_DIR/eval.py -i $M/memm-svc-1 >> $M/eval_output.txt ;} 2>> $M/eval_output.txt
-echo "Training memm svc n=2..."
+echo "Eval memm svc n=2..."
 { time -p python $SCRIPT_DIR/eval.py -i $M/memm-svc-2 >> $M/eval_output.txt ;} 2>> $M/eval_output.txt
-echo "Training memm svc n=3..."
+echo "Eval memm svc n=3..."
 { time -p python $SCRIPT_DIR/eval.py -i $M/memm-svc-3 >> $M/eval_output.txt ;} 2>> $M/eval_output.txt
-echo "Training memm svc n=4..."
+echo "Eval memm svc n=4..."
 { time -p python $SCRIPT_DIR/eval.py -i $M/memm-svc-4 >> $M/eval_output.txt ;} 2>> $M/eval_output.txt
